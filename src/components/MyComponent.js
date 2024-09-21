687a1d6d1d7b7705feab67bbadccd3a09ceffa6d
LeDuc210 <128873899+LeDuck2@users.noreply.github.com>
2024-09-21 22:00:35 +0700

    video 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+import React from 'react';
+
+class MyComponent extends React.Component {
+
+    render(){
+        return (
+            <div>my first component</div>
+        )
+        
+    }
+}
+
+export default MyComponent;
